dcf8dbec6be80f90befbf935b0b788c2b15e8791
Bruno Fauth <149593@upf.br>
2023-04-09 01:00:34 -0300

    bug fixes

--- snow-preview.sh
+++ snow-preview.sh
@@ -6,6 +6,7 @@ case "$file_type" in
     application/x-shellscript) bat --color always --wrap character --style="numbers" "$1";;
     application/x-perl) bat --color always --wrap character --style="numbers" "$1";;
     application/pdf) pdftotext "$1" - ;;
+    application/zip) zipinfo "$1" ;;
     image/*) kitty +kitten icat --clear --transfer-mode file --silent "$1" ;;
     *) echo "No suitable preview for file of type: $file_type"
 esac
